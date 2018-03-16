f573aea7f36a362a4fb61a8733a45611dd4a3ff6
Žiga Osolin <Ziga.Osolin@interblockgaming.com>
2018-03-16 08:33:25 +0100

    Added automatic copy to GodotBuild

--- build-mono.bat
+++ build-mono.bat
@@ -1,3 +1,7 @@
 call scons platform=windows tools=yes module_mono_enabled=yes mono_glue=no
 call bin\godot.windows.tools.32.mono.exe --generate-mono-glue modules/mono/glue
 call scons platform=windows tools=yes module_mono_enabled=yes mono_glue=yes
+call copy bin\godot.windows.tools.32.mono.exe ..\GodotBuild\
+call copy bin\mono-2.0-sgen.dll ..\GodotBuild\
+call copy bin\mscorlib.dll ..\GodotBuild\
+call copy bin\GodotSharpTools.dll ..\GodotBuild\

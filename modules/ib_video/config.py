a1766b31d1b32df4b46a1771b0a71dc115dab9e4
Žiga Osolin <Ziga.Osolin@interblockgaming.com>
2018-04-12 09:20:13 +0200

    Added copy scripts for all ibvideo related stuff to reside in bin

--- modules/ib_video/config.py
+++ modules/ib_video/config.py
@@ -1,10 +1,12 @@
 # config.py
+import os
+import shutil
 
 def can_build(platform):
     return True
 
 def configure(env):
-    env.Append(LINKFLAGS=['modules/ib_video/ffmpeg/win32/lib/avcodec.lib',
+	env.Append(LINKFLAGS=['modules/ib_video/ffmpeg/win32/lib/avcodec.lib',
                           'modules/ib_video/ffmpeg/win32/lib/avdevice.lib',
                           'modules/ib_video/ffmpeg/win32/lib/avfilter.lib',
                           'modules/ib_video/ffmpeg/win32/lib/avformat.lib',
@@ -14,7 +16,15 @@ def configure(env):
                           'modules/ib_video/GL/Win32/glew32.lib',
 						  'd3d9.lib'])
 
-    #env.Append(LIBPATH=["C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86"])
-    
-    print(env["LINKFLAGS"])
-    pass
+	if not os.path.exists('bin'):
+		os.makedirs('bin')
+		
+	shutil.copyfile('modules/ib_video/GL/Win32/glew32.dll', 'bin/glew32.dll')
+	shutil.copyfile('modules/ib_video/ffmpeg/win32/bin/avcodec-57.dll', 'bin/avcodec-57.dll')
+	shutil.copyfile('modules/ib_video/ffmpeg/win32/bin/avdevice-57.dll', 'bin/avdevice-57.dll')
+	shutil.copyfile('modules/ib_video/ffmpeg/win32/bin/avfilter-6.dll', 'bin/avfilter-6.dll')
+	shutil.copyfile('modules/ib_video/ffmpeg/win32/bin/avformat-57.dll', 'bin/avformat-57.dll')
+	shutil.copyfile('modules/ib_video/ffmpeg/win32/bin/avutil-55.dll', 'bin/avutil-55.dll')
+	shutil.copyfile('modules/ib_video/ffmpeg/win32/bin/swresample-2.dll', 'bin/swresample-2.dll')
+	shutil.copyfile('modules/ib_video/ffmpeg/win32/bin/swscale-4.dll', 'bin/swscale-4.dll')
+	pass

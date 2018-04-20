0cffac84c3f6e18b15bd64cedc1d559253b4216c
Nikodem Lokatelj <nikodem.lokatelj@interblockgaming.com>
2018-04-20 09:01:11 +0200

    MOD: Also build the no tools exe

--- build-mono.bat
+++ build-mono.bat
@@ -1,6 +1,7 @@
-call scons platform=windows tools=yes module_mono_enabled=yes mono_glue=no
+call scons p=windows tools=yes module_mono_enabled=yes mono_glue=no
 call bin\godot.windows.tools.32.mono.exe --generate-mono-glue modules/mono/glue
-call scons platform=windows tools=yes module_mono_enabled=yes mono_glue=yes
+call scons p=windows tools=yes module_mono_enabled=yes mono_glue=yes
+call scons p=windows tools=no module_mono_enabled=yes mono_glue=yes
 call copy bin\godot.windows.tools.32.mono.exe ..\GodotBuild\
 call copy bin\mono-2.0-sgen.dll ..\GodotBuild\
 call copy bin\mscorlib.dll ..\GodotBuild\
